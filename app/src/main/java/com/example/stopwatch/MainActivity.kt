6f27b6dd7deb73b72bff03fdfcc16bfc2201a75e
Justin Albert <Jnalbert879@gmail.com>
2022-09-12 08:37:19 -0700

    override each activity life cycle method

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-    // make a classwide static constant in Kotlin
+    // make a class wide static constant in Kotlin
     companion object {
         // all your "static" constants go here
         const val TAG = "MainActivity"
@@ -14,10 +15,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    Log.d(TAG, "onCreate: ")
     }
 
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart: ")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
     }
 }
